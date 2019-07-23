135d10776af8ae38d02c1d7c97d7d24bc5238ee5
adinkins2019 <afdinki@gmail.com>
2019-07-22 21:32:08 -0400

    Modified index.html and resumeBuilder.js files

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,10 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
+$("#main").append("Antonio Dinkins");
+let name="Antonio Dinkins";
+let role = "Full Stack Software Developer";
+let formattedName = HTMLheaderName.replace("%data%", name);
+let formattedRole = HTMLheaderRole.replace("%data%", role);
+$("#header").append(formattedRole);
+$("#header").prepend(formattedName);
